6f7f814bdb64adb1ad8ab5be7dabdb5b21c48211
garrepalliprasad <garrepalli.prasad@gmail.com>
2024-07-01 19:17:37 +0530

    serializer updated to modelserializer

--- restapp/serializers.py
+++ restapp/serializers.py
@@ -1,15 +1,8 @@
 from rest_framework import serializers
 from .models import Student
-class StudentSerializer(serializers.Serializer):
-    name=serializers.CharField(max_length=100)
-    email=serializers.EmailField()
-
-    def create(self, validated_data):
-        return Student.objects.create(**validated_data)
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Student
+        fields=['name','email']
     
-    def update(self, instance, validated_data):
-        instance.name=validated_data.get('name',instance.name)
-        instance.email=validated_data.get('email',instance.email)
-        instance.save()
-        return instance
             
